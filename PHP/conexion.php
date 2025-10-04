7ae2247e204bac369e00f2ff60956e8827b4475d
Jeffrey <jeffcape4@gmail.com>
2025-10-04 15:41:26 -0600

    PHP

--- PHP/conexion.php
+++ PHP/conexion.php
@@ -1,11 +1,12 @@
-<php
+<?php
     $SERVER = "localhost";
     $USER = "root";
     $PASSWORD = "";
     $BD = "servicio_social";
+    $PORT = 4306;
 
-    $conexion = new mysqli_connect($SERVER, $USER, $PASSWORD, $BD);
-    if($conexion->connect_errno){
+    $conexion = new mysqli($SERVER, $USER, $PASSWORD, $BD, $PORT);
+    if($conexion -> connect_errno){
         die("Conexion fallida: " . $conexion->connect_error);
     } else {
         echo "Conexion exitosa";
