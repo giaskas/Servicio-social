be5a0ab31e6601f57cea8f130f82a69d38504e94
giaskas <areyes65@alumnos.uaq.mx>
2025-10-06 22:36:05 -0600

    leer excel

---
+++ PHP/subirArchivo.php
@@ -0,0 +1,35 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (isset($_FILES['archivo']) && $_FILES['archivo']['error'] === UPLOAD_ERR_OK) {
+        $fileTmpPath = $_FILES['archivo']['tmp_name'];
+        $fileName = $_FILES['archivo']['name'];
+        $fileSize = $_FILES['archivo']['size'];
+        $fileType = $_FILES['archivo']['type'];
+        $fileNameCmps = explode(".", $fileName);
+        $fileExtension = strtolower(end($fileNameCmps));
+
+        $allowedfileExtensions = ['xlsx', 'xls', 'csv'];
+        if (in_array($fileExtension, $allowedfileExtensions)) {
+            //vendor/autoload.php es el archivo que carga todas las librerías instaladas con Composer
+
+            require 'vendor/autoload.php';
+            //fileTmpPath es la ruta temporal del archivo subido
+            $spreadsheet = \PhpOffice\PhpSpreadsheet\IOFactory::load($fileTmpPath);
+            
+            $sheet = $spreadsheet->getActiveSheet();
+            $data = $sheet->toArray();
+            print_r($data); 
+
+            //aqui va la logica de empezar a generar el PDF 
+            //se usa FPDI para generar una plantilla de PDF y luego se rellena con los datos extraidos del array del Excel
+
+        } else {
+            echo "Tipo de archivo no permitido. Solo se permiten archivos XLSX, XLS y CSV.";
+        }
+    } else {
+        echo "Error en la subida del archivo. Código de error: " . $_FILES['archivo']['error'];
+    }
+} else {
+    echo "Método de solicitud no válido.";
+}
+?>
